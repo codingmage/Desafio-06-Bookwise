5147c1d5ca6bfb6aedfeafe472c9451812f0da20
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-04-25 11:18:51 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: [
+    'page.tsx',
+    'api.ts',
+    'api.tsx'
+  ],
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+    ],
+  },
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Nunito } from 'next/font/google'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div className={`${Nunito}`}>
+      {/* <div className={`${Nunito.variable}`}> */}
+      <Component {...pageProps} />
+    </div>
+  )
 }
